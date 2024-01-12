4003b399206716ef1f68fee1723c81c2768bfe8b
anushree <anushree9019@gmail.com>
2024-01-12 21:14:53 +0530

    final

--- src/Components/Projects/ProjectData.js
+++ src/Components/Projects/ProjectData.js
@@ -7,6 +7,12 @@ import clone from "./ProjectImg/clone.png";
 import todo from "./ProjectImg/todo.png";
 import form from "./ProjectImg/form.png";
 import weather from "./ProjectImg/weather.png";
+import shoe from "./ProjectImg/shoe.png";
+import interior from "./ProjectImg/interior.png";
+import coffee from "./ProjectImg/coffee.png";
+import weatherdesign from "./ProjectImg/weatherdesign.png";
+import tour from "./ProjectImg/tour.png";
+
 
 
 export const MernProjectData = [
@@ -73,5 +79,41 @@ export const MernProjectData = [
     live: "https://anushree1903.github.io/Form-Validation/",
     github: "https://github.com/anushree1903/Form-Validation",
   },
+  {
+    img: tour,
+    title: "Responsive Tour Website",
+    techstack: "Figma",
+    live: "https://www.figma.com/proto/cNO0UGQSvsCR8eckT7qTun/tour-website?page-id=0%3A1&type=design&node-id=112-10289&viewport=-618%2C-1585%2C0.31&t=QyvDPjJch81HoVGg-1&scaling=min-zoom&mode=design"
+    ,
+  },
+  {
+    img: coffee ,
+    title: "Coffee Shop",
+    techstack: "Figma",
+    live: "https://www.figma.com/proto/3GdBlZYSjZWRTgxTeAVntB/coffee-shop?page-id=0%3A1&type=design&node-id=102-20&viewport=219%2C413%2C0.19&t=AtPw2bznoABVeZj3-1&scaling=min-zoom&mode=design"
+    ,
+  },
+  {
+    img: interior ,
+    title: "Interior Designers Website",
+    techstack: "Figma",
+    live: "https://www.figma.com/proto/MxZTFcEN9l1WjW8YM1cQkP/Interior-Designers?page-id=0%3A1&type=design&node-id=93-2&viewport=411%2C19%2C0.15&t=IflNEbHV6gxWwpZW-1&scaling=scale-down&starting-point-node-id=93%3A2&mode=design"
+    ,
+  },
+  {
+    img: shoe ,
+    title: "Shoe Shop[Animation]",
+    techstack: "FIgma",
+    live: "https://www.figma.com/proto/PiZrLLHqflv5jQbt99pGbq/Nike-shoes?page-id=0%3A1&node-id=6-178&starting-point-node-id=1%3A2&scaling=scale-down&mode=design&t=nC6dLqfrdlddu5f1-1"
+    ,
+  },
+  {
+    img: weatherdesign ,
+    title: "Shoe Shop",
+    techstack: "Figma",
+    live: "https://www.figma.com/file/TplBriX4vQQG0kP8XrDHSy/Weather-app?type=design&node-id=137%3A2&mode=design&t=jpcvqLoSSfRuneL8-1"
+    ,
+  },
+ 
   
 ];
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
   return (
     <div className="flex flex-col justify-center items-center px-3 ">
       <h1 className="text-3xl font-bold text-center border-spacing-2 border-b-2 border-b-indigo-600 my-10">
-        Projects
+        Web Delopment and Web Design Projects
       </h1>
       {data.map(({ value, desc }) => (
         <div key={value} className="mb-32">
