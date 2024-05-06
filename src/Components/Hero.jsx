5d48cb5956a88c4f79564c12707138b048a44b07
anushree <anushree9019@gmail.com>
2024-05-06 11:59:54 +0530

    final

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
             </Button>
           </Link>
           <Link
-            to="https://drive.google.com/file/d/12Wih0QmbpUmpEmmqolWDriirX1uhd912/view?usp=sharing"
+            to="https://drive.google.com/file/d/1gd8cmNMBVS6tYHpqYLBQWkLHwUTBbS38/view?usp=sharing"
             target="_blank"
           >
             <Button
